98b6e9b3fdfe09148f91370d4c77a90d9d145ce9
luoquan <nchkdxlq@gmail.com>
2018-03-09 15:45:04 +0800

    add: upadte headerAnimator

--- SwiftDemo/SwiftDemo/Views/HeaderAnimator.swift
+++ SwiftDemo/SwiftDemo/Views/HeaderAnimator.swift
@@ -11,10 +11,41 @@ import UIKit
 class HeaderAnimator: UIView {
     
     var trigger: CGFloat = 40.0
+    let pullTextLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = UIColor(red: 151.0/255.0, green: 151.0/255.0, blue: 151.0/255.0, alpha: 1.0)
+        label.text = "下拉刷新..."
+        
+        return label
+    }()
     
+    let releaseTextLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = UIColor(red: 151.0/255.0, green: 151.0/255.0, blue: 151.0/255.0, alpha: 1.0)
+        label.text = "释放刷新"
+        return label
+    }()
+    
+    let indicatorView: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        indicator.hidesWhenStopped = true
+        indicator.stopAnimating()
+        return indicator
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        let views: [UIView] = [pullTextLabel, releaseTextLabel, indicatorView]
+        views.forEach { (view) in
+            addSubview(view)
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        addSubview(pullTextLabel)
+        addSubview(releaseTextLabel)
+        addSubview(indicatorView)
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
