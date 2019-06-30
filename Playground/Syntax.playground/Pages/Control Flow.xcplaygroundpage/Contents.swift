9671afb276ce744f1c32ae2b49393802759fd310
luoquan <nchkdxlq.gmail.com>
2019-06-30 12:52:02 +0800

    Emumetation

--- Playground/Syntax.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
+++ Playground/Syntax.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
@@ -31,8 +31,40 @@ for index in 1...5 {
 }
 
 
-let range1 = 0..<60
 
+// 2019/06/29 [New]
+// stride
+let minites = 60
+let miniteInterval = 5
+let __stride = stride(from: 0, to: minites, by: miniteInterval) // 返回一个序列，从start开始，到end结束，但序列中不包含end，序列的步长为stride
+for tickMark in __stride {
+    // render the tick mark every 5 minutes (0, 5, 10, 15 ... 45, 50, 55)
+}
+
+
+//: ## while
+
+let max = 2
+
+// while, 先判断条件，满足条件后再执行循环语句
+var i = 0
+
+while i < max {
+    print(i)
+    i += 1
+}
+// 0
+// 1
+
+
+// 2019/06/29 [New]
+
+// repeat while // 先执行循环语句，再判断条件. 相当于C语言中的  do...while
+var j = 0
+repeat {
+    print(j)
+    j += 1
+} while j < max
 
 // switch
 
---
+++ Playground/Syntax.playground/Pages/Enum.xcplaygroundpage/Contents.swift
@@ -0,0 +1,180 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//: [Next](@next)
+
+/*
+    1. 基本语法
+    2. 关联值
+    3. 原始值
+        1) 隐式类型原始值 Int， String
+    4. 递归枚举
+ */
+
+
+//: 语法
+enum CompassPoint {
+    case north
+    case south
+    case east
+    case west
+}
+
+
+// 使用switch语句匹配枚举值
+
+let dictionToHead = CompassPoint.south
+
+switch dictionToHead {
+case .north:
+    print("Lost of planets have a north")
+case .south:
+    print("Watch out for penguins")
+case .east:
+    print("Where the skies are rires")
+case .west:
+    print("Where the skies are blue")
+}
+
+// 迭代便利枚举值
+
+enum Beverage: CaseIterable {
+    case coffee, tea, juice
+}
+
+let numberOfChoices = Beverage.allCases.count
+print("\(numberOfChoices) beverages available")
+
+// 关联值
+
+enum Barcode {
+    case upc(Int, Int, Int, Int)
+    case qrCode(String)
+}
+
+var productBarcode = Barcode.upc(8, 85909, 51226, 3)
+
+productBarcode = .qrCode("ABCDEFGHIJKLMNOP")
+
+switch productBarcode {
+case .upc(let numberSystem, let manufactuerer, let product, let check):
+        print("UPC: \(numberSystem), \(manufactuerer), \(product), \(check)")
+case .qrCode(let code):
+        print("QR code: \(code)")
+}
+
+// 如果关联值都定义为常亮或者变量，可以把let/var放到枚举名称的前面
+switch productBarcode {
+case let .upc(numberSystem, manufactuerer, product, check):
+    print("UPC: \(numberSystem), \(manufactuerer), \(product), \(check)")
+case .qrCode(let code):
+    print("QR code: \(code)")
+}
+
+// 如果不需要其中的某个关联值，可以使用 _ 忽略它
+switch productBarcode {
+case let .upc(numberSystem, _, _, _):
+    print("UPC: \(numberSystem)")
+case .qrCode(let code):
+    print("QR code: \(code)")
+}
+
+// 原始值 （Raw Values）
+
+enum PokerSuit : Character {
+    case spade = "1"
+    case heart = "2"
+}
+
+// 隐式原始值 （Implicitly Assigned Raw Values）
+/*
+ 如果枚举值的原始值类型时Int, String， Swift会自动分配原始值
+
+ */
+
+// 原始值类型为Int, 第一个case的原始值默认为0， 也可以具体指定，后序case，原始值就递增
+enum Planet: Int {
+    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
+}
+
+
+// 原始值类型为String, 那么每个case的原始值默认为case的名称
+enum Direction: String {
+    case north, south, east, west
+}
+
+print(Direction.north)
+// north
+
+// 等价于下面的写法
+/*
+enum Direction: String {
+    case north = "north"
+    case south = "south"
+    case east = "east"
+    case west = "west"
+}
+*/
+
+
+print(Direction.north) // north
+print(Direction.north.rawValue) // north
+
+
+enum Season: Int {
+    case spring, summer, autumn, winter
+}
+
+
+// 2019/06/30 [New]
+//: ## 递归枚举 （Recursive Enumeration）
+/**
+    枚举中的case的关联值的类型就是该枚举类型, 需要在具体的case前面使用关键字 indirect 声明， 也可以放在整个枚举的前面
+ */
+
+
+enum ArithmeticExpression {
+    case number(Int)
+    indirect case addition(ArithmeticExpression, ArithmeticExpression)
+    indirect case multiplication(ArithmeticExpression, ArithmeticExpression)
+}
+
+/*
+     indirect enum ArithmeticExpression {
+         case number(Int)
+         case addition(ArithmeticExpression, ArithmeticExpression)
+         case multiplication(ArithmeticExpression, ArithmeticExpression)
+     }
+ */
+
+let five = ArithmeticExpression.number(5)
+let four = ArithmeticExpression.number(4)
+let sum = ArithmeticExpression.addition(five, four)
+let product = ArithmeticExpression.multiplication(sum, ArithmeticExpression.number(2))
+
+
+func evaluate(_ expression: ArithmeticExpression) -> Int {
+    switch expression {
+    case let .number(value):
+        return value
+    case let .addition(left, right):
+        return evaluate(left) + evaluate(right)
+    case let .multiplication(left, right):
+        return evaluate(left) * evaluate(right)
+    }
+}
+
+print(evaluate(product))
+// Prints "18"
+
+
+//: ## MemoryLayout
+
+let age = 20
+
+MemoryLayout<Int>.size
+MemoryLayout<Int>.stride
+MemoryLayout<Int>.alignment
+
+
