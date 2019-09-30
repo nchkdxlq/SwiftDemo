f4e1d80dd3a2137b449c118fe8771fd46ac8c351
luoquan <nchkdxlq.gmail.com>
2019-09-30 09:15:26 +0800

    filter

---
+++ SwiftDemo/RxSwiftDemo/Classes/Filter.swift
@@ -0,0 +1,32 @@
+//
+//  Filter.swift
+//  RxSwiftDemo
+//
+//  Created by Knox on 2019/9/29.
+//  Copyright © 2019 luoquan. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+
+struct Filter {
+    
+    let disposeBag = DisposeBag()
+    
+    func filter_entry() {
+        filter_test()
+    }
+    
+    func filter_test() {
+        print("------ filter ------")
+        
+        let seqInt = Observable.of(0, 1, 2, 3, 4)
+        seqInt.filter { num -> Bool in
+            return num % 2 == 0
+        }.subscribe { event in
+            print(event)
+        }.disposed(by: disposeBag)
+    }
+    
+}
