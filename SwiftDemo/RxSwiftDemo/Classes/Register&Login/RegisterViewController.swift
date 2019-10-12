b909c13afff0e5055ad3e79519cad90436b9b5c6
luoquan <nchkdxlq@gmail.com>
2019-10-12 19:31:53 +0800

    add register

---
+++ SwiftDemo/RxSwiftDemo/Classes/Register&Login/RegisterViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RegisterViewController.swift
+//  RxSwiftDemo
+//
+//  Created by Knox on 2019/10/12.
+//  Copyright © 2019 luoquan. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+
+class RegisterViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "注册"
+        
+        
+        
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SwiftDemo/RxSwiftDemo/Classes/Register&Login/RegisterViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterViewModel.swift
+//  RxSwiftDemo
+//
+//  Created by Knox on 2019/10/12.
+//  Copyright © 2019 luoquan. All rights reserved.
+//
+
+import Foundation
+
+
+struct RegisterViewModel {
+    
+    
+    
+}
