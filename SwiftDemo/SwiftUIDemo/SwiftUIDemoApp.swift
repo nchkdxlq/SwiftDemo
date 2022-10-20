168e347500c5f3224e875d4e1a6ff9fda5cb3eac
luoquan <nchkdxlq@gmail.com>
2022-10-20 08:22:16 +0800

    feat: add SwiftUI Target

---
+++ SwiftDemo/SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by luoquan on 2022/10/20.
+//  Copyright © 2022 luoquan. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
